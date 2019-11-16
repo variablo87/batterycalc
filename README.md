ad0e2525f9598c015e0f8b0684a012415dc39caf
Norbert Hesse <norbert.hesse@deveritec.com>
2019-11-16 11:00:31 +0100

    * change UI * PWA support * settings saved at browser

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # batterycalc
 Battery Life Calculator
+
+https://variablo87.github.io/batterycalc/
+
+
+
+## Inspired by:
+
+https://oregonembedded.com/batterycalc.htm
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
+function getSettings(){
+    if (typeof(Storage) !== "undefined") {
+      console.log("Web Storage support");
+    } else {
+      console.log("No Web Storage support");
+    }
+
+    if (localStorage.localJSON){
+        fillForm(JSON.parse(localStorage.localJSON));
+        console.log("restore settings");
+    }
+    else{
+        console.log("no settings stored");
+    }
+}
+
+function saveSettings(){
+    localStorage.localJSON = toJSONString(document.getElementById("form1").elements);
+}
+
+function toJSONString( form ) {
+	var obj = {};
+	var elements = form.querySelectorAll( "input, select, textarea" );
+	for( var i = 0; i < elements.length; ++i ) {
+		var element = elements[i];
+		var name = element.name;
+		var value = element.value;
+
+		if( name ) {
+			obj[ name ] = value;
+		}
+	}
+	return JSON.stringify( obj );
+}
+
+function fillForm( json ){
+    var tempJson = json;
+    for (var item in tempJson) {
+        document.getElementsByName(item)[0].value = tempJson[item];
+    }
+}
+
 //General Array Function
 function MakeArray(n) {
   this.length = n;
@@ -8,33 +50,39 @@ function MakeArray(n) {
 
 //Day of Week Function
 function compute(form) {
-
-  var C = Number(C);
-  var As = Number(As);
-  var Aw = Number(Aw);
-	
-  var Wph = Number(Wph);
-  var Wt = Number(Wt);
-
-
-  var C = form.capacity.value;
-  var As = form.sleep_c.value;
-  var Aw = form.wake_c.value;
-	
-  var Wph = form.wake_n.value;
-  var Wt = form.wake_t.value;
-	
-  var c = C * 0.85;
-  //milliseconds are in an hour
-  var msph = 3600000;
-  var Twph = Wph * Wt
-  var Tsph = msph - Wph
-  var Aavg = ((Aw*Twph)+(As*Tsph))/msph
-  var days = (c/Aavg)/24
-  var years = days/365
-  
-  form.result1.value = Math.round(days*100)/100;
-  form.result2.value = Math.round(years*100)/100;
+    var msph = 3600000;
+    
+    var capRat = form.capacity.value+form.n_battery.value; 
+    var offma = form.sleep_c.value; 
+    
+    var onms1 = form.wake_t_1.value*form.wake_n_1.value;  
+    var onma1 = form.wake_c_1.value; 
+    if (onms1>msph)   
+        {   onms1=msph;  }  
+    
+    var onms2 = form.wake_t_2.value*form.wake_n_2.value;  
+    var onma2 = form.wake_c_2.value; 
+    if (onms2>msph)   
+        {   onms2=msph;  }  
+        
+    var onms3 = form.wake_t_3.value*form.wake_n_3.value;  
+    var onma3 = form.wake_c_3.value; 
+    if (onms3>msph)   
+        {   onms3=msph;  }  
+        
+    var modRat = capRat*0.85;
+    
+    var offms = msph-onms1-onms2-onms3;
+    
+    var realma=((offma*offms)+(onma1*onms1)+(onma2*onms2)+(onma3*onms3))/msph; 
+    var lifeda = (modRat/realma)/24; 
+    var lifeyr = lifeda/365; 
+    
+    form.result1.value = Math.round(lifeda)/10;
+    form.result2.value = Math.round(lifeyr)/10;
+    
+    localStorage.localJSON = toJSONString(form);
+    window.location.href = "#results";
 }
 
          // end script -->
